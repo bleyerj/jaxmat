53c109bf8a150bb32d224c3cac74720d59f7a0bf
Jeremy Bleyer <jeremy.bleyer@enpc.fr>
2025-07-11 23:30:56 +0200

    Add config

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Setup file for the jaxmat package.
+
+@author: Jeremy Bleyer, Ecole Nationale des Ponts et Chaussées, IP-Paris, Laboratoire Navier
+@email: jeremy.bleyer@enpc.fr
+"""
+from setuptools import setup
+
+setup()
