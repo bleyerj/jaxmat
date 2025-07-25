5449d8cabd62b77afc8f91358a9b362297dc0567
Jeremy Bleyer <jeremy.bleyer@enpc.fr>
2025-07-25 18:03:26 +0200

    Fix state test

--- tests/test_state.py
+++ tests/test_state.py
@@ -7,16 +7,13 @@ import pytest
 class MyState(MechanicalState):
     additional_state: jax.Array
 
-
-state = MyState(
-    stress=jnp.zeros((6,)), strain=jnp.zeros((6,)), additional_state=jnp.zeros((3,))
-)
+    def __init__(self, additional_state):
+        super().__init__()
+        self.additional_state = additional_state
 
 
 def test_state():
-    state = MyState(
-        stress=jnp.zeros((6,)), strain=jnp.zeros((6,)), additional_state=jnp.zeros((3,))
-    )
+    state = MyState(additional_state=jnp.zeros((3,)))
     state = state.update(strain=jnp.ones((6,)))
     assert jnp.allclose(state.strain, jnp.ones((6,)))
     state = state.add(stress=2 * jnp.ones((6,)), strain=0.5 * jnp.ones((6,)))
@@ -26,17 +23,13 @@ def test_state():
 
 @pytest.mark.parametrize("Nbatch", [1, 10, 100])
 def test_batching(Nbatch):
-    state = MyState(
-        stress=jnp.zeros((6,)), strain=jnp.zeros((6,)), additional_state=jnp.zeros((3,))
-    )
+    state = MyState(additional_state=jnp.zeros((3,)))
     batched_state = make_batched(state, Nbatch)
     assert batched_state.strain.shape == (Nbatch, 6)
     assert batched_state.stress.shape == (Nbatch, 6)
     assert batched_state.additional_state.shape == (Nbatch, 3)
 
     state = MyState(
-        stress=jnp.zeros((6,)),
-        strain=jnp.zeros((6,)),
         additional_state=jnp.zeros((3, 3)),
     )
     batched_state = make_batched(state, Nbatch)
