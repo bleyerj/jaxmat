6a84bc7752936268d5d32c5c59222e6c471c5139
Jeremy Bleyer <jeremy.bleyer@enpc.fr>
2025-07-25 17:59:46 +0200

    More tensor updates

--- jaxmat/tensors/__init__.py
+++ jaxmat/tensors/__init__.py
@@ -1,2 +1,12 @@
 from .generic_tensors import Tensor2, SymmetricTensor2, SymmetricTensor4
-from .tensor_utils import polar, stretch_tensor, dev, skew, sym
+from .tensor_utils import (
+    polar,
+    stretch_tensor,
+    dev,
+    skew,
+    sym,
+    to_mat,
+    to_vect,
+    dev_vect,
+    eigenvalues,
+)
--- jaxmat/tensors/tensor_utils.py
+++ jaxmat/tensors/tensor_utils.py
@@ -1,8 +1,8 @@
 from functools import partial
 import jax
 import jax.numpy as jnp
-from .linear_algebra import _sqrtm
-from . import Tensor2, SymmetricTensor2
+from .linear_algebra import _sqrtm, eig33
+from . import Tensor2, SymmetricTensor2, SymmetricTensor4
 
 
 # These functions apply to tensors
@@ -34,11 +34,79 @@ def tr(A):
 
 
 def dev(A):
-    print(A)
     Id = SymmetricTensor2.identity()
     return A - tr(A) / A.dim * Id
+
+
+def dev_vect(A):
+    K = SymmetricTensor4.K().array
+    return K @ A
 
 
 def stretch_tensor(F):
     """Computes the strech tensor U = sqrtm(F.T @ F)."""
     return polar(F)[1]
+
+
+@jax.custom_jvp
+def eigenvalues(sig):
+    eigvals, eigendyads = eig33(sig)
+    return eigvals
+
+
+@eigenvalues.defjvp
+def eigenvalues_jvp(primals, tangents):
+    (sig,) = primals
+    (dsig,) = tangents
+    eigvals, eigendyads = eig33(sig)
+    deig = jnp.tensordot(eigendyads, dsig)
+    return eigvals, deig
+
+
+def to_mat(x):
+    # FIXME: should be removed when working with Tensors
+    if len(x) == 6:
+        return jnp.array(
+            [
+                [x[0], x[3] / jnp.sqrt(2), x[4] / jnp.sqrt(2)],
+                [x[3] / jnp.sqrt(2), x[1], x[5] / jnp.sqrt(2)],
+                [x[4] / jnp.sqrt(2), x[5] / jnp.sqrt(2), x[2]],
+            ]
+        )
+    else:
+        return jnp.array(
+            [
+                [x[0], x[3], x[5]],
+                [x[4], x[1], x[7]],
+                [x[6], x[8], x[2]],
+            ]
+        )
+
+
+def to_vect(X, symmetry=False):
+    # FIXME: should be removed when working with Tensors
+    if symmetry:
+        return jnp.array(
+            [
+                X[0, 0],
+                X[1, 1],
+                X[2, 2],
+                jnp.sqrt(2) * X[0, 1],
+                jnp.sqrt(2) * X[0, 2],
+                jnp.sqrt(2) * X[1, 2],
+            ]
+        )
+    else:
+        return jnp.array(
+            [
+                X[0, 0],
+                X[1, 1],
+                X[2, 2],
+                X[0, 1],
+                X[1, 0],
+                X[0, 2],
+                X[2, 0],
+                X[1, 2],
+                X[2, 1],
+            ]
+        )
