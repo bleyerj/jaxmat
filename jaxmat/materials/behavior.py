8ac25487c4e0487b548d64fe4072d56f7218eef2
Jeremy Bleyer <jeremy.bleyer@enpc.fr>
2025-08-28 16:39:32 +0200

    Improve batching of states

--- jaxmat/materials/behavior.py
+++ jaxmat/materials/behavior.py
@@ -1,4 +1,6 @@
 from abc import abstractmethod
+import jax
+import jax.numpy as jnp
 import equinox as eqx
 import optimistix as optx
 from jaxmat.state import (
@@ -11,12 +13,14 @@ from jaxmat.solvers import DEFAULT_SOLVERS
 
 
 class AbstractBehavior(eqx.Module):
+    internal: eqx.AbstractVar[AbstractState]
     solver: optx.AbstractRootFinder = eqx.field(
         static=True, init=False, default=DEFAULT_SOLVERS[0]
     )
     adjoint: optx.AbstractAdjoint = eqx.field(
         static=True, init=False, default=DEFAULT_SOLVERS[1]
     )
+    _batch_size: jax.Array = eqx.field(default=0.0, init=False, converter=jnp.asarray)
 
     @abstractmethod
     def constitutive_update(self, eps, state, dt):
@@ -27,24 +31,29 @@ class AbstractBehavior(eqx.Module):
             eqx.filter_vmap(self.constitutive_update, in_axes=(0, 0, None))
         )(eps, state, dt)
 
-
-class SmallStrainBehavior(AbstractBehavior):
-    internal: eqx.AbstractVar[AbstractState]
-
-    def init_state(self, Nbatch=None):
-        state = SmallStrainState(internal=self.internal)
+    def _init_state(self, cls, Nbatch=None):
+        state = cls(internal=self.internal)
         if Nbatch is None:
-            return state
+            if (
+                len(self._batch_size.shape) == 1
+            ):  # Handle the case where the material has already been batched
+                # we first batch cls without internals
+                Nbatch = self._batch_size.shape[0]
+                state = make_batched(cls(), Nbatch)
+                # we reaffect the already batched internals
+                state = eqx.tree_at(lambda s: s.internal, state, self.internal)
+                return state
+            else:
+                return cls(internal=self.internal)
         else:
             return make_batched(state, Nbatch)
+
+
+class SmallStrainBehavior(AbstractBehavior):
+    def init_state(self, Nbatch=None):
+        return self._init_state(SmallStrainState, Nbatch)
 
 
 class FiniteStrainBehavior(AbstractBehavior):
-    internal: eqx.AbstractVar[AbstractState]
-
     def init_state(self, Nbatch=None):
-        state = FiniteStrainState(internal=self.internal)
-        if Nbatch is None:
-            return state
-        else:
-            return make_batched(state, Nbatch)
+        return self._init_state(FiniteStrainState, Nbatch)
