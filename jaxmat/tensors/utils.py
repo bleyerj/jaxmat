26b0ef641fc7685791e93fa722c55187c06ae92f
Jeremy Bleyer <jeremy.bleyer@enpc.fr>
2025-08-28 16:39:01 +0200

    use safe norm from optax

--- jaxmat/tensors/utils.py
+++ jaxmat/tensors/utils.py
@@ -1,12 +1,14 @@
 import jax.numpy as jnp
+import optax
 
 
 def safe_sqrt(x, eps=1e-16):
-    return jnp.sqrt(jnp.clip(x, min=eps))
+    nonzero_x = jnp.where(x > eps, x, eps)
+    return jnp.where(x > eps, jnp.sqrt(nonzero_x), eps)
 
 
-def safe_norm(x, eps=1e-16):
-    return jnp.sqrt(jnp.maximum(jnp.trace(x.T @ x), eps))
+def safe_norm(x, eps=1e-16, **kwargs):
+    return optax.safe_norm(x, eps, **kwargs)
 
 
 def FischerBurmeister(x, y):
