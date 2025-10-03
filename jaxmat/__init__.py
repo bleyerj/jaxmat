9ea71bb45e607da94691ff029406b0d9dc0bec38
Jeremy Bleyer <jeremy.bleyer@enpc.fr>
2025-10-03 16:32:18 +0200

    Add paths in init

--- jaxmat/__init__.py
+++ jaxmat/__init__.py
@@ -4,3 +4,9 @@ jax.config.update("jax_enable_x64", True)  # use double-precision
 jax.config.update("jax_debug_nans", True)  # raise when encountering nan
 
 from jaxmat.solvers.gauss_newton_ls import GaussNewtonLineSearch
+
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parent
+DEMOS_DIR = PROJECT_ROOT.parent / "demos"
+DATA_DIR = DEMOS_DIR / "_data"
