ea7419efa3cf007635fe4631383ae3732d854a34
Jeremy Bleyer <jeremy.bleyer@enpc.fr>
2025-10-04 23:38:38 +0200

    Missed one

--- demos/getting_started.py
+++ demos/getting_started.py
@@ -12,15 +12,32 @@
 #     name: python3
 # ---
 
-# +
+# # Getting started with `jaxmat`
+#
+# In this tutorial, we show how to define and evaluate a simple elastoplastic behavior with nonlinear isotropic hardening.
+#
+# We first import `jax` and may specify whether we want to run on the CPU or the GPU. We will also need the `equinox` from which we use `Module`s to define the different behavior bricks.
+
 import jax
 jax.config.update("jax_platform_name", "cpu")
 import jax.numpy as jnp
 import equinox as eqx
 import jaxmat.materials as jm
-import matplotlib.pyplot as plt
 
+# ## Defining a behavior
+#
+# In the `jaxmat.materials` module, various material models such as elasticity or von Mises plasticity are already available. We will use `LinearElasticIsotropic` for the elastic part and `vonMisesIsotropicHardening` for the elastoplastic model. The latter expects a yield stress module representing the isotropic hardening. In the following, we use a Voce-type exponential hardening:
+#
+# $$
+# \sigma_\text{Y}(p) = \sigma_0 + (\sigma_\text{u}-\sigma_0)\exp(-bp)
+# $$
+#
+# where $\sigma_0$ (resp. $\sigma_u$) is the initial (resp. final) yield stress and $b$ is a hardening parameter controlling the rate of convergence from $\sigma_0$ to $\sigma_u$ as a function of the cumulated plastic strain $p$.
+#
+# For this purpose, we simply define an `equinox.Module` with the material parameter attributes $(\sigma_0,\sigma_u, b)$ and a `__call__` function which evaluates the current yield stress as a function of the cumulated plastic strain $p$.
 
+# +
+elasticity = jm.LinearElasticIsotropic(E=200e3, nu=0.25)
 
 class VoceHardening(eqx.Module):
     sig0: float
@@ -29,17 +46,22 @@ class VoceHardening(eqx.Module):
     def __call__(self, p):
         return self.sig0 + (self.sigu-self.sig0)*(1-jnp.exp(-self.b*p))
 
-
-elasticity = jm.LinearElasticIsotropic(E=200e3, nu=0.25)
 hardening = VoceHardening(sig0=350., sigu=500.0, b=1e3)
+# -
 
+# We now define the final material behavior of type `vonMisesIsotropicHardening`. We can check that it is a module containing an `elastic_model` submodule and a `yield_stress` subdomule. Each of these submodule holds its own material properties. The elastic submodule also allows to access the material elastic shear modulus for instance, which is 80 GPa here.
+
+# +
 material = jm.vonMisesIsotropicHardening(elastic_model=elasticity, yield_stress=hardening)
+print(material)
 print(material.elastic_model.__dict__)
 print(material.yield_stress.__dict__)
-print(material)
 
 mu = elasticity.mu
 print(f"\nShear modulus = {1e-3*mu} GPa")
+# -
+
+# Since `hardening` is a JAX function, we can also compute its derivative using `jax.grad` to obtain the corresponding hardening modulus. Below, we evaluate this modulus on an array of values of cumulated plastic strain and plot the result.
 
 # +
 hardening_modulus = jax.grad(hardening)
@@ -47,18 +69,25 @@ hardening_modulus = jax.grad(hardening)
 p = jnp.linspace(0, 5e-3, 100)
 H = jax.vmap(hardening_modulus)
 
-plt.figure(figsize=(12, 4))
+
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(12, 5))
 plt.subplot(1, 2, 1)
 plt.plot(p, hardening(p), '-C0')
 plt.gca().set_ylim(bottom=0)
 plt.xlabel("Equivalent plastic strain $p$")
-plt.ylabel("Yield stress $R(p)$ [MPa]");
+plt.ylabel("Yield stress $\sigma_Y(p)$ [MPa]");
 plt.subplot(1, 2, 2)
 plt.plot(p, 1e-3*H(p), '-C3')
 plt.gca().set_ylim(bottom=0)
 plt.xlabel("Equivalent plastic strain $p$")
-plt.ylabel("Hardening modulus $R'(p)$ [GPa]");
+plt.ylabel("Hardening modulus $H(p)=\sigma_Y'(p)$ [GPa]");
 # -
+
+# ## Mechanical states
+#
+# In order to evaluate the response of a mechanical behavior, we need a mechanical state. Each material provides an `init_state` method to initialize its correponding mechanical state with default initial values (usually 0). Below, we see that the present state contrains a `strain` and a `stress`, each of them being a symmetric 2nd-rank tensor. In addition, it also contains an `internal` field, which is itself a state consisting of many internal state variables. In the present case, both the cumulated plastic strain $p$ and the total plastic strain $\bepsp$ are declared as internal state variables.
 
 state = material.init_state()
 print(state.__dict__)
@@ -115,13 +144,13 @@ for i, gamma in enumerate(gamma_list):
     p = p.at[i].set(state.internal.p)
     mu_tang = mu_tang.at[i].set(Ctang[0, 1, 0, 1])
 
-# Note that the tangent operator is the derivative of a `SymmetricTensor2` with respect to a `SymmetricTensor2` which is formally equivalent to a 4th-rank tensor. Its component can thus be accessed as `Ctang[i, j, k, l]`. Collecting the value of the tangent shear modulus from `Ctang[0, 1, 0, 1]`, its evolution clearly shows a first constant phase in the elastic regime where $\mu_\text{tang}=\mu=80\text{ GPa}$. We see the sudden drop at the onset of plasticity which is due to the finite initial hardening slope $R'(0)$. We now that the material (elastoplastic) tangent operator is given by:
+# Note that the tangent operator is the derivative of a `SymmetricTensor2` with respect to a `SymmetricTensor2` which is formally equivalent to a 4th-rank tensor. Its component can thus be accessed as `Ctang[i, j, k, l]`. Collecting the value of the tangent shear modulus from `Ctang[0, 1, 0, 1]`, its evolution clearly shows a first constant phase in the elastic regime where $\mu_\text{tang}=\mu=80\text{ GPa}$. We see the sudden drop at the onset of plasticity which is due to the finite initial hardening slope $R'(0)$. We know that the material (elastoplastic) tangent operator is given by:
 #
 # $$\mathbb{C}^\text{ep} = 3\kappa\mathbb{J} + 2\mu\left(\mathbb{K}-\dfrac{3\mu}{3\mu+R'(p)}\boldsymbol{n}\otimes\boldsymbol{n}\right)$$
 # where $\boldsymbol{n}$ is the unit normal vector in the direction of the plastic flow. For pure shear conditions, this gives the elastoplastic tangent shear modulus:
 # $$
 # \mu^\text{ep} = \mu\left(1-\dfrac{3\mu}{3\mu+R'(p)}\right) = \mu\dfrac{R'(p)}{3\mu+R'(p)}
-# $$ 
+# $$
 
 plt.plot(gamma_list, mu_tang*1e-3, "-k", label="Consistent")
 mu_ep = jnp.full_like(gamma_list, mu)
