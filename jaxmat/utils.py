8f99eca0a83f21aa1bc38972d9470fdf2e01a9ad
Jeremy Bleyer <jeremy.bleyer@enpc.fr>
2025-10-22 14:41:53 +0200

    Add utils

--- jaxmat/utils.py
+++ jaxmat/utils.py
@@ -8,3 +8,32 @@ def default_value(value, dtype=jnp.float64, **kwargs):
     return eqx.field(
         converter=lambda x: jnp.asarray(x, dtype=dtype), default=value, **kwargs
     )
+
+
+def enforce_dtype(dtype=jnp.float64, **kwargs):
+    """Initialize and convert a field with default `value` of imposed `dtype`."""
+    return eqx.field(converter=lambda x: jnp.asarray(x, dtype=dtype), **kwargs)
+
+
+def partition_by_node_names(model, freeze_names):
+    """
+    Partition an Equinox model into (trainable, static) where
+    attributes listed in `freeze_names` are frozen (moved to static).
+    """
+
+    # Start with array-vs-nonarray partition
+    trainable, static = eqx.partition(model, eqx.is_array)
+
+    for name in freeze_names:
+        sel = lambda m, name=name: getattr(m, name)
+
+        # move out of trainable
+        trainable = eqx.tree_at(
+            sel, trainable, replace=None, is_leaf=lambda x: x is None
+        )
+        # copy original value into static
+        static = eqx.tree_at(
+            sel, static, replace=getattr(model, name), is_leaf=lambda x: x is None
+        )
+
+    return trainable, static
