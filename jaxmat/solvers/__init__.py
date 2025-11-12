d9e9455583a4a0e9aeffee440a249d8a9f9ecdd4
Jeremy Bleyer <jeremy.bleyer@enpc.fr>
2025-11-12 15:47:47 +0100

    Default solver parameters to well_posed

--- jaxmat/solvers/__init__.py
+++ jaxmat/solvers/__init__.py
@@ -6,12 +6,12 @@ from .custom_optimistix_solvers import (
     NewtonTrustRegion,
 )
 
-linear_solver = lx.AutoLinearSolver(well_posed=False)
+DEFAULT_LINEAR_SOLVER = lx.AutoLinearSolver(well_posed=True)
 DEFAULT_SOLVERS = (
     optx.Newton(
         rtol=1e-8,
         atol=1e-8,
-        linear_solver=linear_solver,
+        linear_solver=DEFAULT_LINEAR_SOLVER,
     ),
-    optx.ImplicitAdjoint(linear_solver=linear_solver),
+    optx.ImplicitAdjoint(linear_solver=DEFAULT_LINEAR_SOLVER),
 )
